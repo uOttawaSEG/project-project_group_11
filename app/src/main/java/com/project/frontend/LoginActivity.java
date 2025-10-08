3f15533045a41ff21cf4976a43aac1da177ccdaa
jakobdubeau <jakobdubeau@gmail.com>
2025-10-07 23:50:52 -0400

    more screen switching

--- app/src/main/java/com/project/frontend/LoginActivity.java
+++ app/src/main/java/com/project/frontend/LoginActivity.java
@@ -1,4 +1,5 @@
 package com.project.frontend;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,16 +20,18 @@ public class LoginActivity extends AppCompatActivity {
         EditText emailField = findViewById(R.id.editTextTextEmailAddress);
         EditText passwordField = findViewById(R.id.editTextTextPassword);
         Button loginButton = findViewById(R.id.button2);
+        Button createAccountButton = findViewById(R.id.button7);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View view) {
+        loginButton.setOnClickListener(view -> {
 
                 // convert editable text objects to strings
                 String emailInput = emailField.getText().toString();
                 String passwordInput = passwordField.getText().toString();
-
-            }
         });
+
+        createAccountButton.setOnClickListener(view -> {
+            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+        });
+
     }
 }
--- app/src/main/java/com/project/frontend/RegisterActivity.java
+++ app/src/main/java/com/project/frontend/RegisterActivity.java
@@ -1,4 +1,5 @@
 package com.project.frontend;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,5 +14,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button accountCreated = findViewById(R.id.button3);
+
+        accountCreated.setOnClickListener(view -> { // for future this is a lambda expression
+            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+        });
     }
 }
