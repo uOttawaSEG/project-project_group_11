a45308ba73d8e8aebb4b3cf33fd163de6e19d135
jakobdubeau <jakobdubeau@gmail.com>
2025-10-07 23:38:52 -0400

    created register screen ui

--- app/src/main/java/com/project/frontend/RegisterActivity.java
+++ app/src/main/java/com/project/frontend/RegisterActivity.java
@@ -1,4 +1,17 @@
 package com.project.frontend;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegisterActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
 }
